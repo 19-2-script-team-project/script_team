00bd3a15a55c650404e0ed9994f0a1e8006299d7
Haky-KPU <ha52529@naver.com>
2019-05-29 03:55:10 +0900

    중간 발표

--- Python_TermProject/Python_TermProject/API_Parsing.py
+++ Python_TermProject/Python_TermProject/API_Parsing.py
@@ -1,5 +1,7 @@
         
         #5GHaky xhpMDatlPQddXaIoAqs3tqgHNuJY0Q2IwG_3ztonj0WSyeo
+        #kmmjEmPIS8vz7KeNTCEJegBOZPIObRoxMoQh8vHoXGzm
+        #uyLDF9li-2kVsPToVrhGDcwSUXkr007gQ5wK9_a0C_HzBCQ
 import webbrowser
 import urllib.request
 import urllib.parse
@@ -16,21 +18,15 @@ import http.client
 #JSON 파일을 딕셔너리로 파싱하기
 
 class RiotApiParsing:
+    # 초기화 : 서버, Key / PlayerID, AccountID는 함수에서 받아옴.
     def __init__(self):
         self.__Server = "kr.api.riotgames.com"
-        self.__ApiKey = "RGAPI-60be92e4-4d94-4073-a51e-5c4aaa0cd82d"
+        self.__ApiKey = "RGAPI-3144c3aa-6726-42c0-a894-7adee400c429"
         self.__PlayerID = ""
         self.__AccountID = ""
-    def getPlayerIDByName(self,PlayerName):
+        self.__championData = []
 
-        if (PlayerName == ""):
-            print("비어있는입력")
-            return
-        #PlayerName = "5GHaky"
-        #ApiKey = "RGAPI-a6ad83f9-60dd-468e-b68d-b3dc9b24446e"
-        #server = "kr.api.riotgames.com"  # 물음표까지 다써도됌
-        #파싱 ..
-        #UserName = urllib.parse.quote(UserName)
+    def getPlayerIDByName(self,PlayerName):
 
         conn = http.client.HTTPSConnection(self.__Server)
 
@@ -39,16 +35,11 @@ class RiotApiParsing:
         req = conn.getresponse()
         result = req.read().decode('utf-8')
         jsonTempData = json.loads(result)
-        if len(jsonData.jsonData["rows"]) == 0:
-            return null;
 
         self.__PlayerID = jsonTempData['id']
         self.__AccountID = jsonTempData['accountId']
 
     def getPlayerMasteryByPlayerID(self):
-        if (PlayerID == ""):
-            print("비어있는입력")
-            return
                 
         conn = http.client.HTTPSConnection(self.__Server)
 
@@ -58,11 +49,26 @@ class RiotApiParsing:
         result = req.read().decode('utf-8')
         return json.loads(result)
 
-    def getMatchsByPlayerID(self):
-        if (PlayerID == ""):
-            print("비어있는입력")
-            return
-                
+    def getPlayerLeagueByPlayerID(self):
+        conn = http.client.HTTPSConnection(self.__Server)
+
+        conn.request("GET","/lol/league/v4/entries/by-summoner/" + self.__PlayerID +  "?api_key=" + self.__ApiKey)
+
+        req = conn.getresponse()
+        result = req.read().decode('utf-8')
+        return json.loads(result)
+
+    def getPlayingGameByPlayerID(self):
+        conn = http.client.HTTPSConnection(self.__Server)
+
+        conn.request("GET","/lol/spectator/v4/active-games/by-summoner/" + self.__PlayerID +  "?api_key=" + self.__ApiKey)
+
+        req = conn.getresponse()
+        result = req.read().decode('utf-8')
+        return json.loads(result)
+
+    def getMatchsByAccountID(self):
+        #champion, queue, season, endIndex, beginIndex
         conn = http.client.HTTPSConnection(self.__Server)
         filterOptions = "?"
         conn.request("GET","/lol/match/v4/matchlists/by-account/" + self.__AccountID + filterOptions + "api_key=" + self.__ApiKey)
@@ -72,56 +78,51 @@ class RiotApiParsing:
         return json.loads(result)
 
     def getAllChampionsData(self):
+
         conn = http.client.HTTPSConnection("ddragon.leagueoflegends.com")
         conn.request("GET","/cdn/9.10.1/data/ko_KR/champion.json")
         req = conn.getresponse()
         result = req.read().decode('utf-8')
-
-    #http://ddragon.leagueoflegends.com/cdn/9.10.1/data/ko_KR/champion.json
-  
-	#이미지 파일 얻어오는코드
-    def getImgByChampionsName(self):
+        return json.loads(result)
         
-        url = "http://ddragon.leagueoflegends.com/cdn/9.10.1/img/champion/" + "Azir" + ".png"
+
+        
+        print("완료")
+         
+	#이미지 파일 얻어오는코드
+    def getImgByChampionsName(self, championName):
+        url = "http://ddragon.leagueoflegends.com/cdn/9.10.1/img/champion/" + championName + ".png"
         #http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Azir_0.jpg
 
-        outpath = "images/"
-        outfile = "image_" + "Azir" + ".png"
+        outPath = "images/"
+        outFile = "image_" + championName + ".png"
 
-        if not os.path.isdir(outpath):
-            os.makedirs(outpath)
+        if not os.path.isdir(outPath):
+            os.makedirs(outPath)
 
-        urllib.request.urlretrieve(url, outpath + outfile)
-	#여기까지
+        urllib.request.urlretrieve(url, outPath + outFile)
+    
+    
 
-        print(jsonData)
-        webbrowser.open_new(url)
+class championData:
+    
+    def __init__(self, data):
+        #self.__championData = []
+        temp = []
+        for i in data['data'].items():
+            #self.__championData.append(i)
+           temp.append(i)
+        self.__championData = tuple(temp)
 
-        pass
+        #for i in range(len(self.__championData)):
+            #self.getImgByChampionsName(championData[i][0]) #img 호출 코드
+            #print(self.__championData[i][0])
 
+    def printChampionData(self):
+        for i in self.__championData:
+            print(i)
+        self.__championData[0]
 
-
-
-class ParsingDataOfItems:
-    def __init__(self,JSON):
-        self.jsonData = json.loads(JSON) #string 형태의 JSON 객체를 딕셔너리로 바꾼다
-
-
-        if len(self.jsonData["rows"]) == 0:
-            return
-
-        self.itemID = self.jsonData["rows"][0]["itemId"]
-        self.itemName = self.jsonData["rows"][0]["itemName"]
-        self.itemRarity = self.jsonData["rows"][0]["itemRarity"]
-        self.itemType = self.jsonData["rows"][0]["itemType"]
-        self.itemDetail = str(self.jsonData["rows"][0]["itemTypeDetail"])
-        self.itemAvailableLevel = str(self.jsonData["rows"][0]["itemAvailableLevel"])
-
-    def __str__(self):
-        if len(self.jsonData["rows"]) == 0:
-            return ""
-        return " [아이템 고유코드 : " + self.itemID + "]\n [아이템이름 : " + self.itemName + "]\n [아이템 레어도 : " + self.itemRarity + "]\n [아이템 타입 : " + self.itemType + "]\n [아이템 타입상세 : " + self.itemDetail + "]\n [아이템 착용레벨 : " + self.itemAvailableLevel + "]\n"
-
-
-RiotApiParsing().GetItemInfoFromMarket("흑천의 주인 - 대검")
-
+abc = RiotApiParsing()
+abc.getPlayerIDByName("5GHaky")
+a = championData(abc.getAllChampionsData())
