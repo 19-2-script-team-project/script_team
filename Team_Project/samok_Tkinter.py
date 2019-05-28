9cd4432c01dde448ae276e1509bdc4d24c07a425
Haky-KPU <ha52529@naver.com>
2019-05-28 22:35:07 +0900

    commit

--- Team_Project/samok_Tkinter.py
+++ Team_Project/samok_Tkinter.py
@@ -3,14 +3,19 @@ from tkinter import *
 turn = 1
 Table
 L1
+winstone
+direction
+endcount = 0
 def judgement(row, column, color, cnt = 0, dir = 0 ):
-    print(cnt)
     if cnt == 4:
+        global direction
+        direction = dir
         return color
     if dir == 0:
         cnt += 1
         if judgement(row, column, color, cnt, dir = 1) != 0 or judgement(row, column, color, cnt, dir = 2) != 0 or judgement(row, column, color, cnt, dir = 3) != 0 or judgement(row, column, color, cnt, dir = 4) != 0 or judgement(row, column, color, cnt, dir = 5) != 0: 
-            print("color : ", color)
+            global winstone
+            winstone = (row, column)
             return color
         return 0
         #Left #Left Down #Down #Right Down #Right
@@ -57,25 +62,29 @@ class Cell(Canvas) :
         self.column = column
 
         Canvas.__init__(self, container, width = 50, height = 50, bg = 'white')
-        self.create_oval(10,10,40,40,fill = "white", tag = "empty")
+        self.create_oval(10,10,40,40,fill = "white", tag = "grim")
         self.bind("<Button-1>", self.click)
 
     def push(self):
         global turn
+        global endcount
+        endcount += 1
         if(turn == 1):
             self.state = 1
-            self.delete("empty")
-            self.create_oval(10,10,40,40,fill = "red")
+            self.delete("grim")
+            self.create_oval(10,10,40,40,fill = "red", tag = "grim")
             turn = 2
         elif(turn == 2):
             self.state = 2
-            self.delete("empty")
-            self.create_oval(10,10,40,40,fill = "yellow")
+            self.delete("grim")
+            self.create_oval(10,10,40,40,fill = "yellow", tag = "grim")
             turn = 1
+        if(endcount == 41):
+            L1.configure(text = ")
+            turn = -1
 
     def click(self, event):
         global Table
-        print(self.row, " ",self.column)
         if(self.state == 0):
             if(self.row == 5):
                 self.push()
@@ -91,7 +100,115 @@ class Cell(Canvas) :
         if(judge != 0):
             L1.configure(text = "{0} 승리! 게임이 끝났습니다.".format(judge))
             turn = -1
+            self.blink()
 
+    def blink(self):
+        global winstone
+        global Table
+        row, column = winstone
+        print(direction)
+        print("WWW")
+        while True:
+            self.after(200)
+            self.update()
+            if direction == 1:
+                Table[row][column].delete("grim")
+                Table[row][column - 1].delete("grim")
+                Table[row][column - 2].delete("grim")
+                Table[row][column - 3].delete("grim")
+
+                Table[row][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column - 1].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column - 2].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column - 3].create_oval(10,10,40,40,fill = "red", tag = "grim")
+            elif direction == 2:
+                Table[row][column].delete("grim")
+                Table[row+1][column - 1].delete("grim")
+                Table[row+2][column - 2].delete("grim")
+                Table[row+3][column - 3].delete("grim")
+
+                Table[row][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+1][column - 1].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+2][column - 2].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+3][column - 3].create_oval(10,10,40,40,fill = "red", tag = "grim")
+            elif direction == 3:
+                Table[row][column].delete("grim")
+                Table[row+1][column].delete("grim")
+                Table[row+2][column].delete("grim")
+                Table[row+3][column].delete("grim")
+
+                Table[row][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+1][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+2][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+3][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+            elif direction == 4:
+                Table[row][column].delete("grim")
+                Table[row+1][column+1].delete("grim")
+                Table[row+2][column+2].delete("grim")
+                Table[row+3][column+3].delete("grim")
+
+                Table[row][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+1][column+1].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+2][column+2].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row+3][column+3].create_oval(10,10,40,40,fill = "red", tag = "grim")
+            elif direction == 5:
+                Table[row][column].delete("grim")
+                Table[row][column+1].delete("grim")
+                Table[row][column+2].delete("grim")
+                Table[row][column+3].delete("grim")
+
+                Table[row][column].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column+1].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column+2].create_oval(10,10,40,40,fill = "red", tag = "grim")
+                Table[row][column+3].create_oval(10,10,40,40,fill = "red", tag = "grim")
+
+            self.after(200)
+            self.update()
+            if direction == 1:
+                Table[row][column].delete("grim")
+                Table[row][column - 1].delete("grim")
+                Table[row][column - 2].delete("grim")
+                Table[row][column - 3].delete("grim")
+                Table[row][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column - 1].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column - 2].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column - 3].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+            elif direction == 2:
+                Table[row][column].delete("grim")
+                Table[row+1][column - 1].delete("grim")
+                Table[row+2][column - 2].delete("grim")
+                Table[row+3][column - 3].delete("grim")
+                Table[row][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+1][column - 1].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+2][column - 2].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+3][column - 3].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+            elif direction == 3:
+                Table[row][column].delete("grim")
+                Table[row+1][column].delete("grim")
+                Table[row+2][column].delete("grim")
+                Table[row+3][column].delete("grim")
+                Table[row][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+1][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+2][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+3][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+            elif direction == 4:
+                Table[row][column].delete("grim")
+                Table[row+1][column+1].delete("grim")
+                Table[row+2][column+2].delete("grim")
+                Table[row+3][column+3].delete("grim")
+                Table[row][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+1][column+1].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+2][column+2].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row+3][column+3].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+            elif direction == 5:
+                Table[row][column].delete("grim")
+                Table[row][column+1].delete("grim")
+                Table[row][column+2].delete("grim")
+                Table[row][column+3].delete("grim")
+                Table[row][column].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column+1].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column+2].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
+                Table[row][column+3].create_oval(10,10,40,40,fill = "yellow", tag = "grim")
 
 class Samok:
     def __init__(self):
